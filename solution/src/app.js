94fdb268c00bd7f901b054eb360e4ff07366da65
abyl10 <abylay.kairatbek@nu.edu.kz>
2022-09-25 00:17:09 +0600

    changes

--- solution/src/app.js
+++ solution/src/app.js
@@ -12,59 +12,55 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Loader } from '@googlemaps/js-api-loader';
-import * as THREE from 'three';
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { Loader } from "@googlemaps/js-api-loader";
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
 const apiOptions = {
-  apiKey: 'YOUR API KEY',
-  version: "beta"
+  apiKey: "AIzaSyA3EhOaGjf6RCQv_A8KQwZf-cRXvOw54lA",
+  version: "beta",
 };
 
 const mapOptions = {
-  "tilt": 0,
-  "heading": 0,
-  "zoom": 18,
-  "center": { lat: 35.6594945, lng: 139.6999859 },
-  "mapId": "YOUR MAP ID"    
-}
+  tilt: 0,
+  heading: 0,
+  zoom: 18,
+  center: { lat: 35.6594945, lng: 139.6999859 },
+  mapId: "79a3d2948d1dd2ef",
+};
 
-async function initMap() {    
+async function initMap() {
   const mapDiv = document.getElementById("map");
   const apiLoader = new Loader(apiOptions);
   await apiLoader.load();
   return new google.maps.Map(mapDiv, mapOptions);
 }
 
-
-function initWebGLOverlayView(map) {  
+function initWebGLOverlayView(map) {
   let scene, renderer, camera, loader;
   const webGLOverlayView = new google.maps.WebGLOverlayView();
-  
-  webGLOverlayView.onAdd = () => {   
+
+  webGLOverlayView.onAdd = () => {
     // set up the scene
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera();
-    const ambientLight = new THREE.AmbientLight( 0xffffff, 0.75 ); // soft white light
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.75); // soft white light
     scene.add(ambientLight);
     const directionalLight = new THREE.DirectionalLight(0xffffff, 0.25);
     directionalLight.position.set(0.5, -1, 0.5);
     scene.add(directionalLight);
-  
-    // load the model    
-    loader = new GLTFLoader();               
+
+    // load the model
+    loader = new GLTFLoader();
     const source = "pin.gltf";
-    loader.load(
-      source,
-      gltf => {      
-        gltf.scene.scale.set(25,25,25);
-        gltf.scene.rotation.x = 180 * Math.PI/180; // rotations are in radians
-        scene.add(gltf.scene);           
-      }
-    );
-  }
-  
-  webGLOverlayView.onContextRestored = ({gl}) => {    
+    loader.load(source, (gltf) => {
+      gltf.scene.scale.set(25, 25, 25);
+      gltf.scene.rotation.x = (180 * Math.PI) / 180; // rotations are in radians
+      scene.add(gltf.scene);
+    });
+  };
+
+  webGLOverlayView.onContextRestored = ({ gl }) => {
     // create the three.js renderer, using the
     // maps's WebGL rendering context.
     renderer = new THREE.WebGLRenderer({
@@ -74,48 +70,48 @@ function initWebGLOverlayView(map) {
     });
     renderer.autoClear = false;
 
-    // wait to move the camera until the 3D model loads    
-    loader.manager.onLoad = () => {        
+    // wait to move the camera until the 3D model loads
+    loader.manager.onLoad = () => {
       renderer.setAnimationLoop(() => {
         map.moveCamera({
-          "tilt": mapOptions.tilt,
-          "heading": mapOptions.heading,
-          "zoom": mapOptions.zoom
-        });            
-        
-        // rotate the map 360 degrees 
+          tilt: mapOptions.tilt,
+          heading: mapOptions.heading,
+          zoom: mapOptions.zoom,
+        });
+
+        // rotate the map 360 degrees
         if (mapOptions.tilt < 67.5) {
-          mapOptions.tilt += 0.5
+          mapOptions.tilt += 0.5;
         } else if (mapOptions.heading <= 360) {
           mapOptions.heading += 0.2;
         } else {
-          renderer.setAnimationLoop(null)
+          renderer.setAnimationLoop(null);
         }
-      });        
-    }
-  }
+      });
+    };
+  };
 
-  webGLOverlayView.onDraw = ({gl, transformer}) => {
+  webGLOverlayView.onDraw = ({ gl, transformer }) => {
     // update camera matrix to ensure the model is georeferenced correctly on the map
     const latLngAltitudeLiteral = {
-        lat: mapOptions.center.lat,
-        lng: mapOptions.center.lng,
-        altitude: 120
-    }
+      lat: mapOptions.center.lat,
+      lng: mapOptions.center.lng,
+      altitude: 120,
+    };
 
     const matrix = transformer.fromLatLngAltitude(latLngAltitudeLiteral);
     camera.projectionMatrix = new THREE.Matrix4().fromArray(matrix);
-    
-    webGLOverlayView.requestRedraw();      
-    renderer.render(scene, camera);                  
+
+    webGLOverlayView.requestRedraw();
+    renderer.render(scene, camera);
 
     // always reset the GL state
     renderer.resetState();
-  }
+  };
   webGLOverlayView.setMap(map);
 }
 
-(async () => {        
+(async () => {
   const map = await initMap();
-  initWebGLOverlayView(map);    
+  initWebGLOverlayView(map);
 })();
